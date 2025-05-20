d7d1300ba473508e239ee10cd2eb09999931a802
Zhuo <nicodiazan@gmail.com>
2025-05-20 15:55:38 +0200

    Add a text truncator composable

--- pages/test.vue
+++ pages/test.vue
@@ -20,6 +20,23 @@ onMounted(async () => {
 		.insert("\n");
 });
 
+const text = ref(null);
+const textChild = ref(null);
+
+onMounted(async () => {
+	console.log("boutta truncate");
+	console.log(text.value);
+	if (!text.value || !textChild.value) return;
+	console.log("almost truncatin");
+	useTruncateText(
+		text.value,
+		textChild.value,
+		4,
+		"show less.",
+		"... show more"
+	);
+});
+
 function handleSelectionChange(r) {
 	range.value = r;
 }
@@ -36,30 +53,66 @@ function getContentLength() {
 
 <template>
 	<div>
+		<p ref="test">
+			<span ref="testChild"
+				>Very long text should be truncate use js. Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate.Very long text should be truncate.Very
+				long text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.Very long
+				text should be truncate use js. Very long text should be
+				truncate.Very long text should be truncate.Very long text should
+				be truncate.Very long text should be truncate.Very long text
+				should be truncate.Very long text should be truncate.</span
+			>
+		</p>
 
-        <ClientOnly>
-            <MdEditor
-            v-if="content"
-            ref="quillRef"
-            v-model="content"
-            v-model:contentMarkdown="contentMarkdown"
-            :readOnly="false"
-            @selection-change="handleSelectionChange"
-            @text-change="handleTextChange"
-			/>
-        </ClientOnly>
-        <hr>
 		<ClientOnly>
-            <MdEditor
-            v-if="content"
-            ref="quillRef"
-            v-model="content"
-            v-model:contentMarkdown="contentMarkdown"
-            :readOnly="false"
-            @selection-change="handleSelectionChange"
-            @text-change="handleTextChange"
+			<MdEditor
+				v-if="content"
+				ref="quillRef"
+				v-model="content"
+				v-model:contentMarkdown="contentMarkdown"
+				:readOnly="false"
+				@selection-change="handleSelectionChange"
+				@text-change="handleTextChange"
 			/>
-        </ClientOnly>
+		</ClientOnly>
+		<hr />
+		<ClientOnly>
+			<MdEditor
+				v-if="content"
+				ref="quillRef"
+				v-model="content"
+				v-model:contentMarkdown="contentMarkdown"
+				:readOnly="false"
+				@selection-change="handleSelectionChange"
+				@text-change="handleTextChange"
+			/>
+		</ClientOnly>
 
 		<div class="controls">
 			<button class="controls-right" @click="getContentLength">
