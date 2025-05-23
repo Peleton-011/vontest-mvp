740e0237cc4e3a37795d726db2e7d97b9099b7a5
Zhuo <nicodiazan@gmail.com>
2025-05-23 17:38:44 +0200

    Add and improve basic pages for viewing, editing and deleting proposals

--- pages/proposals/[vontestId]/new/index.vue
+++ pages/proposals/[vontestId]/new/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const route = useRoute();
-const id = route.params.id as string;
+const id = route.params.vontestId as string;
 </script>
 <template>
     <FormProposal :vontestId="id"/>
