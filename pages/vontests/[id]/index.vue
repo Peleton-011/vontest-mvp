59db8511aa6093254401f32d7a2c3b54e58e400d
Zhuo <nicodiazan@gmail.com>
2025-05-23 19:16:27 +0200

    Add pagination and searching to the proposals list under a vontest

--- pages/vontests/[id]/index.vue
+++ pages/vontests/[id]/index.vue
@@ -11,17 +11,22 @@ const user = useSupabaseUser();
 
 const vontestId = route.params.id as string;
 
-const vontest = ref<Vontest | null>(null);
-
-const { proposals, fetchProposals, submitProposal } =
-	useProposals(vontestId);
+const { proposals, submitProposal } = useProposals(vontestId);
 
 const { fetchVontest } = useVontests();
 
-onMounted(async () => {
-	vontest.value = (await fetchVontest(vontestId)) || null;
-	await fetchProposals();
-});
+const vontest = ref<Vontest | null>((await fetchVontest(vontestId)) || null);
+
+const pageSize = 6;
+
+const {
+	searchQuery,
+	currentPage,
+	totalPages,
+	paginatedItems: paginatedProposals,
+	goToNextPage,
+	goToPrevPage,
+} = usePaginationSearch<Vontest>(proposals, pageSize);
 </script>
 
 <template>
@@ -30,15 +35,14 @@ onMounted(async () => {
 
 		<UCard v-if="vontest">
 			<template #header>
-                <div class="flex items-center justify-between">
-
-                    <h1 class="text-2xl font-bold">{{ vontest.title }}</h1>
-                    <OptionsDropdown
-					v-if="vontest.created_by === user?.id"
-					@edit="navigateTo(`/vontests/${vontestId}/edit`)"
-					@delete="navigateTo(`/vontests/${vontestId}/delete`)"
-                    />
-                </div>
+				<div class="flex items-center justify-between">
+					<h1 class="text-2xl font-bold">{{ vontest.title }}</h1>
+					<OptionsDropdown
+						v-if="vontest.created_by === user?.id"
+						@edit="navigateTo(`/vontests/${vontestId}/edit`)"
+						@delete="navigateTo(`/vontests/${vontestId}/delete`)"
+					/>
+				</div>
 			</template>
 			<p
 				class="text-gray-400 markdown-body ql-editor"
@@ -61,56 +65,81 @@ onMounted(async () => {
 				</div>
 			</template>
 		</UCard>
+	</section>
+	<section class="p-6 max-w-4xl mx-auto text-white">
+		<div v-if="proposals.length > 0" class="space-y-4">
+			<div class="flex items-center justify-between">
+				<h2 class="text-2xl font-semibold">Browse Proposals</h2>
 
-		<!-- Proposal List -->
-		<div>
-			<div v-if="proposals.length">
-				<h2 class="text-2xl font-semibold my-4">Proposals</h2>
-				<UCard
-					v-for="proposal in proposals"
-					:key="proposal.id"
-					class="group bg-neutral-800 mb-4 hover:bg-neutral-700 [&>*]:group-hover:border-neutral-700 [&>*]:transition transition"
-				>
-					<template #header>
-						<div class="text-lg font-bold">
-							{{ proposal.title }}
-						</div>
-					</template>
-					<p
-						class="text-gray-400 markdown-body ql-editor"
-						v-html="proposal.description"
-					></p>
-
-					<template #footer>
-						<div class="flex justify-between items-center gap-2">
-							<small class="text-gray-500"
-								>Submitted on
-								{{
-									new Date(
-										proposal.created_at ?? ""
-									).toLocaleString()
-								}}</small
-							>
-
-							<NuxtLink
-								:to="`/vontests/${vontest?.id}/vote`"
-								class="w-1/3"
-							>
-								<UButton
-									block
-									variant="outline"
-									icon="i-lucide-vote"
-								>
-									Vote
-								</UButton>
-							</NuxtLink>
-						</div>
-					</template>
-				</UCard>
+				<div class="flex justify-center items-center gap-2">
+					<!-- Search Bar -->
+					<div class="flex items-center justify-between">
+						<UInput
+							v-model="searchQuery"
+							placeholder="Search Proposals..."
+							icon="i-lucide-search"
+							class="w-full max-w-md"
+						/>
+					</div>
+					<!-- Add Proposal -->
+					<UButton
+						label="New Proposal"
+						color="primary"
+						variant="subtle"
+						to="/proposals/new"
+					/>
+				</div>
 			</div>
-			<div v-else class="text-gray-500 text-center p-4">
-				No proposals yet. Be the first to suggest something.
+
+			<!-- Proposal Cards -->
+
+			<StaticCard
+				v-for="proposal in paginatedProposals"
+				:key="proposal.id"
+				:to="`/proposals/${proposal.id}`"
+				:title="proposal.title || ''"
+				:description="proposal.description || ''"
+				:created="proposal.created_at || ''"
+			>
+				<template v-slot:actions>
+					<NuxtLink
+						:to="`/vontests/${vontest?.id}/vote`"
+						class="w-1/3"
+					>
+						<UButton block variant="outline" icon="i-lucide-vote">
+							Vote
+						</UButton>
+					</NuxtLink>
+				</template>
+			</StaticCard>
+
+			<!-- Pagination Controls -->
+			<div
+				v-if="totalPages > 1"
+				class="mt-8 flex justify-center items-center space-x-2 text-sm"
+			>
+				<UButton
+					size="sm"
+					icon="i-lucide-chevron-left"
+					:disabled="currentPage === 1"
+					:variant="currentPage === 1 ? 'soft' : 'solid'"
+					@click="goToPrevPage"
+				/>
+				<span class="text-gray-400"
+					>Page {{ currentPage }} of {{ totalPages }}</span
+				>
+				<UButton
+					size="sm"
+					icon="i-lucide-chevron-right"
+					:disabled="currentPage === totalPages"
+					:variant="currentPage === totalPages ? 'soft' : 'solid'"
+					@click="goToNextPage"
+				/>
 			</div>
 		</div>
+		<div v-else class="text-gray-500 text-center p-4">
+			No proposals yet. Be the first to suggest something.
+		</div>
 	</section>
+
 </template>
