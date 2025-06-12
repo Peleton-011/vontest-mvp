12b46f4e3dfd06c5999507e32a0dafb9920222ad
Nico Diaz <nicodiazan@gmail.com>
2025-06-12 09:56:22 +0200

    Update supabase types

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,235 +1,359 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
 
 export type Database = {
-  public: {
-    Tables: {
-      proposals: {
-        Row: {
-          created_at: string | null
-          created_by: string | null
-          description: string | null
-          id: string
-          title: string | null
-          vontest_id: string | null
+    public: {
+      Tables: {
+        comment_links: {
+          Row: {
+            child_id: string
+            parent_id: string
+            thread_id: string
+          }
+          Insert: {
+            child_id?: string
+            parent_id?: string
+            thread_id?: string
+          }
+          Update: {
+            child_id?: string
+            parent_id?: string
+            thread_id?: string
+          }
+          Relationships: [
+            {
+              foreignKeyName: "comment_links_child_id_thread_id_fkey"
+              columns: ["child_id", "thread_id"]
+              isOneToOne: false
+              referencedRelation: "comments"
+              referencedColumns: ["id", "thread_id"]
+            },
+            {
+              foreignKeyName: "comment_links_parent_id_thread_id_fkey"
+              columns: ["parent_id", "thread_id"]
+              isOneToOne: false
+              referencedRelation: "comments"
+              referencedColumns: ["id", "thread_id"]
+            },
+            {
+              foreignKeyName: "comment_links_thread_id_fkey"
+              columns: ["thread_id"]
+              isOneToOne: false
+              referencedRelation: "proposals"
+              referencedColumns: ["id"]
+            },
+          ]
         }
-        Insert: {
-          created_at?: string | null
-          created_by?: string | null
-          description?: string | null
-          id?: string
-          title?: string | null
-          vontest_id?: string | null
+        comments: {
+          Row: {
+            comment: string
+            created_at: string
+            id: string
+            thread_id: string
+            user_id: string
+          }
+          Insert: {
+            comment: string
+            created_at?: string
+            id?: string
+            thread_id?: string
+            user_id?: string
+          }
+          Update: {
+            comment?: string
+            created_at?: string
+            id?: string
+            thread_id?: string
+            user_id?: string
+          }
+          Relationships: [
+            {
+              foreignKeyName: "comments_thread_id_fkey"
+              columns: ["thread_id"]
+              isOneToOne: false
+              referencedRelation: "proposals"
+              referencedColumns: ["id"]
+            },
+            {
+              foreignKeyName: "comments_user_id_fkey1"
+              columns: ["user_id"]
+              isOneToOne: false
+              referencedRelation: "profiles"
+              referencedColumns: ["id"]
+            },
+          ]
         }
-        Update: {
-          created_at?: string | null
-          created_by?: string | null
-          description?: string | null
-          id?: string
-          title?: string | null
-          vontest_id?: string | null
+        profiles: {
+          Row: {
+            avatar_url: string | null
+            created_at: string | null
+            id: string
+            username: string | null
+          }
+          Insert: {
+            avatar_url?: string | null
+            created_at?: string | null
+            id: string
+            username?: string | null
+          }
+          Update: {
+            avatar_url?: string | null
+            created_at?: string | null
+            id?: string
+            username?: string | null
+          }
+          Relationships: []
         }
-        Relationships: [
-          {
-            foreignKeyName: "proposals_vontest_id_fkey"
-            columns: ["vontest_id"]
-            isOneToOne: false
-            referencedRelation: "vontests"
-            referencedColumns: ["id"]
-          },
-        ]
+        proposals: {
+          Row: {
+            created_at: string | null
+            created_by: string | null
+            description: string | null
+            id: string
+            title: string | null
+            vontest_id: string | null
+          }
+          Insert: {
+            created_at?: string | null
+            created_by?: string | null
+            description?: string | null
+            id?: string
+            title?: string | null
+            vontest_id?: string | null
+          }
+          Update: {
+            created_at?: string | null
+            created_by?: string | null
+            description?: string | null
+            id?: string
+            title?: string | null
+            vontest_id?: string | null
+          }
+          Relationships: [
+            {
+              foreignKeyName: "proposals_created_by_fkey"
+              columns: ["created_by"]
+              isOneToOne: false
+              referencedRelation: "profiles"
+              referencedColumns: ["id"]
+            },
+            {
+              foreignKeyName: "proposals_vontest_id_fkey"
+              columns: ["vontest_id"]
+              isOneToOne: false
+              referencedRelation: "vontests"
+              referencedColumns: ["id"]
+            },
+          ]
+        }
+        vontests: {
+          Row: {
+            created_at: string | null
+            created_by: string | null
+            description: string | null
+            id: string
+            title: string | null
+          }
+          Insert: {
+            created_at?: string | null
+            created_by?: string | null
+            description?: string | null
+            id?: string
+            title?: string | null
+          }
+          Update: {
+            created_at?: string | null
+            created_by?: string | null
+            description?: string | null
+            id?: string
+            title?: string | null
+          }
+          Relationships: [
+            {
+              foreignKeyName: "vontests_created_by_fkey"
+              columns: ["created_by"]
+              isOneToOne: false
+              referencedRelation: "profiles"
+              referencedColumns: ["id"]
+            },
+          ]
+        }
+        votes: {
+          Row: {
+            id: string
+            points: number | null
+            proposal_id: string | null
+            user_id: string | null
+            vontest_id: string | null
+          }
+          Insert: {
+            id?: string
+            points?: number | null
+            proposal_id?: string | null
+            user_id?: string | null
+            vontest_id?: string | null
+          }
+          Update: {
+            id?: string
+            points?: number | null
+            proposal_id?: string | null
+            user_id?: string | null
+            vontest_id?: string | null
+          }
+          Relationships: [
+            {
+              foreignKeyName: "user_id_fkey"
+              columns: ["user_id"]
+              isOneToOne: false
+              referencedRelation: "profiles"
+              referencedColumns: ["id"]
+            },
+            {
+              foreignKeyName: "votes_proposal_id_fkey"
+              columns: ["proposal_id"]
+              isOneToOne: false
+              referencedRelation: "proposals"
+              referencedColumns: ["id"]
+            },
+            {
+              foreignKeyName: "votes_vontest_id_fkey"
+              columns: ["vontest_id"]
+              isOneToOne: false
+              referencedRelation: "vontests"
+              referencedColumns: ["id"]
+            },
+          ]
+        }
       }
-      vontests: {
-        Row: {
-          created_at: string | null
-          created_by: string | null
-          description: string | null
-          id: string
-          title: string | null
-        }
-        Insert: {
-          created_at?: string | null
-          created_by?: string | null
-          description?: string | null
-          id?: string
-          title?: string | null
-        }
-        Update: {
-          created_at?: string | null
-          created_by?: string | null
-          description?: string | null
-          id?: string
-          title?: string | null
-        }
-        Relationships: []
+      Views: {
+        [_ in never]: never
       }
-      votes: {
-        Row: {
-          id: string
-          points: number | null
-          proposal_id: string | null
-          user_id: string | null
-          vontest_id: string | null
+      Functions: {
+        create_comment_with_links: {
+          Args: {
+            in_thread_id: string
+            in_user_id: string
+            in_comment_text: string
+            in_parent_ids: string[]
+          }
+          Returns: string
         }
-        Insert: {
-          id?: string
-          points?: number | null
-          proposal_id?: string | null
-          user_id?: string | null
-          vontest_id?: string | null
-        }
-        Update: {
-          id?: string
-          points?: number | null
-          proposal_id?: string | null
-          user_id?: string | null
-          vontest_id?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "votes_proposal_id_fkey"
-            columns: ["proposal_id"]
-            isOneToOne: true
-            referencedRelation: "proposals"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "votes_vontest_id_fkey"
-            columns: ["vontest_id"]
-            isOneToOne: false
-            referencedRelation: "vontests"
-            referencedColumns: ["id"]
-          },
-        ]
       }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
+      Enums: {
+        [_ in never]: never
+      }
+      CompositeTypes: {
+        [_ in never]: never
+      }
     }
   }
-}
-
-type DefaultSchema = Database[Extract<keyof Database, "public">]
-
-export type Tables<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
-  }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+  
+  type DefaultSchema = Database[Extract<keyof Database, "public">]
+  
+  export type Tables<
+    DefaultSchemaTableNameOrOptions extends
+      | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+      | { schema: keyof Database },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+      schema: keyof Database
     }
-    ? R
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+      ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+          Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+      : never = never,
+  > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+    ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
         Row: infer R
       }
       ? R
       : never
-    : never
-
-export type TablesInsert<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
-  }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+    : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+          DefaultSchema["Views"])
+      ? (DefaultSchema["Tables"] &
+          DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+          Row: infer R
+        }
+        ? R
+        : never
+      : never
+  
+  export type TablesInsert<
+    DefaultSchemaTableNameOrOptions extends
+      | keyof DefaultSchema["Tables"]
+      | { schema: keyof Database },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+      schema: keyof Database
     }
-    ? I
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+      ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+      : never = never,
+  > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+    ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
         Insert: infer I
       }
       ? I
       : never
-    : never
-
-export type TablesUpdate<
-  DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
-  }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+    : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+      ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+          Insert: infer I
+        }
+        ? I
+        : never
+      : never
+  
+  export type TablesUpdate<
+    DefaultSchemaTableNameOrOptions extends
+      | keyof DefaultSchema["Tables"]
+      | { schema: keyof Database },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+      schema: keyof Database
     }
-    ? U
-    : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+      ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+      : never = never,
+  > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+    ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
         Update: infer U
       }
       ? U
       : never
-    : never
-
-export type Enums<
-  DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
-  EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
-  }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
-> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
-
-export type CompositeTypes<
-  PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
-  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
-  }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
-
-export const Constants = {
-  public: {
-    Enums: {},
-  },
-} as const
+    : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+      ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+          Update: infer U
+        }
+        ? U
+        : never
+      : never
+  
+  export type Enums<
+    DefaultSchemaEnumNameOrOptions extends
+      | keyof DefaultSchema["Enums"]
+      | { schema: keyof Database },
+    EnumName extends DefaultSchemaEnumNameOrOptions extends {
+      schema: keyof Database
+    }
+      ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+      : never = never,
+  > = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+    ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+      ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+      : never
+  
+  export type CompositeTypes<
+    PublicCompositeTypeNameOrOptions extends
+      | keyof DefaultSchema["CompositeTypes"]
+      | { schema: keyof Database },
+    CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+      schema: keyof Database
+    }
+      ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+      : never = never,
+  > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+    : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+      ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+      : never
+  
+  export const Constants = {
+    public: {
+      Enums: {},
+    },
+  } as const
+  
