7700236ae4595e3f7fe7af9b0e4c73496c9302ca
Zhuo <nicodiazan@gmail.com>
2025-06-26 10:26:32 +0200

    Continue developing drawer for advanced comment editing

--- pages/test.vue
+++ pages/test.vue
@@ -49,17 +49,20 @@ function getContentLength() {
 	const len = quillRef.value?.getLength?.();
 	alert(len ?? "Quill instance not ready");
 }
+
+const comment = ref("");
+
+// :handle="false"
 </script>
 
 <template>
 	<div>
 		<UDrawer
 			:overlay="false"
+			:modal="false"
 			should-scale-background
 			:set-background-color-on-scale="false"
-			title="Drawer with description"
-			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            inset
+			inset
 		>
 			<UButton
 				label="Open"
@@ -69,9 +72,20 @@ function getContentLength() {
 			/>
 
 			<template #content>
-				<div>
-                CONTENT
-                </div>
+				<div class="p-4 grid grid-cols-3">
+					<div class="col-span-2">
+						<ClientOnly>
+							<!-- class="w-full rounded-[calc(var(--ui-radius)*1.5)] border-0 placeholder:text-(--ui-text-dimmed) focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors px-2.5 py-1.5 text-sm gap-1.5 text-(--ui-text-highlighted) bg-(--ui-bg) ring ring-inset ring-(--ui-border-accented) focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-[--ui-primary]" -->
+							<MdEditor
+								id="description"
+								v-model="comment.value"
+							/>
+						</ClientOnly>
+					</div>
+                    <div class="col-span-1">
+                        <Placeholder />
+                    </div>
+				</div>
 			</template>
 		</UDrawer>
 
