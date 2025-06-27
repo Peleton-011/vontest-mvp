a55d5894680688d1d872fb31096585e06c853ad2
Zhuo <nicodiazan@gmail.com>
2025-06-27 17:27:04 +0200

    Add RefsList element to use in the advanced editor

---
+++ components/ui/RefsList.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { Ref } from "./CommentRefs.vue";
+
+const props = defineProps<{
+	refs: Ref[];
+}>();
+</script>
+<template>
+	<ul class="list-disc list-inside text-gray-400">
+		<li v-for="ref in props.refs" :key="ref.id" class="my-1">
+			<UiCommentRef :reference="ref" />
+		</li>
+	</ul>
+</template>
