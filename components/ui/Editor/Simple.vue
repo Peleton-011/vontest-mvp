99c669bf5dcf94480a5931fd1be1904084bab0ec
Zhuo <nicodiazan@gmail.com>
2025-06-28 12:43:11 +0200

    Implement the editor options into the Simple editor component

--- components/ui/Editor/Simple.vue
+++ components/ui/Editor/Simple.vue
@@ -20,12 +20,17 @@ const emit = defineEmits<{
 </script>
 <template>
 	<div class="flex flex-col">
-		<div class="mb-4">
+		<div class="relative mb-4">
 			<textarea
 				v-model="localCommentText"
 				class="w-full p-2 rounded bg-gray-800"
 				rows="3"
 				placeholder="Write your reply..."
+			/>
+			<UiCommentEditorOptions
+            class="absolute top-2 right-2 z-10"
+				:is-advanced="false"
+				@toggle-editor="emit('toggle-editor')"
 			/>
 			<div class="text-right mt-2">
 				<UButton
@@ -39,14 +44,6 @@ const emit = defineEmits<{
 					@click="emit('cancel')"
 				/>
 			</div>
-		</div>
-		<div class="flex justify-end">
-			<UButton
-				class="w-1/3 text-center"
-				variant="subtle"
-				label="Advanced Editor"
-				@click="emit('toggle-editor')"
-			/>
 		</div>
 	</div>
 </template>
