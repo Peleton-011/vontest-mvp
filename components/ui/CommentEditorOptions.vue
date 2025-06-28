af3296bb643b9c207e43551c12bc1d0c33b1481e
Zhuo <nicodiazan@gmail.com>
2025-06-28 09:18:50 +0200

    Add basic component for comment editor options

---
+++ components/ui/CommentEditorOptions.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from "@nuxt/ui";
+
+const props = defineProps<{
+	onDelete: (e: Event) => void;
+	onEdit: (e: Event) => void;
+}>();
+
+const items = ref<DropdownMenuItem[]>([
+	{
+		label: "Edit",
+		icon: "i-lucide-edit",
+		onSelect: (e) => props.onEdit(e),
+	},
+	{
+		label: "Delete",
+		icon: "i-lucide-trash",
+		onSelect: (e) => props.onDelete(e),
+	},
+]);
+</script>
+
+<template>
+	<UDropdownMenu
+		:items="items"
+		:content="{
+			align: 'end',
+			side: 'bottom',
+			sideOffset: 8,
+		}"
+		:ui="{
+			content: 'w-12',
+		}"
+	>
+		<UButton
+			icon="i-lucide-menu"
+			color="neutral"
+			variant="ghost"
+		/>
+	</UDropdownMenu>
+</template>
