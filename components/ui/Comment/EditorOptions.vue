71f79ce7bd0727fd5a25c858d8bb362c34399a85
Zhuo <nicodiazan@gmail.com>
2025-06-28 12:37:21 +0200

    Minor tweaks for UI/UX

--- components/ui/Comment/EditorOptions.vue
+++ components/ui/Comment/EditorOptions.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import type { DropdownMenuItem } from "@nuxt/ui";
 
+const props = defineProps<{
+	isAdvanced: boolean;
+}>();
+
 const emit = defineEmits<{
 	(e: "toggle-editor"): void;
 }>();
@@ -9,9 +13,9 @@ const { settings, fetchSettings, updateSettings } = useUserSettings();
 
 onMounted(fetchSettings);
 
-const isAdvanced = computed(() => {
-	return settings?.value?.defaultEditor === "advanced";
-});
+// const isAdvanced = computed(() => {
+// 	return settings?.value?.defaultEditor === "advanced";
+// });
 
 const items = computed<DropdownMenuItem[]>(() => {
 	const advanced = {
@@ -27,20 +31,30 @@ const items = computed<DropdownMenuItem[]>(() => {
 	};
 
 	const toggleDefault = {
-		label: "Change Default Editor to " + isAdvanced ? "Simple" : "Advanced",
+		label:
+			"Change default editor to " +
+			(props.isAdvanced ? "Simple" : "Advanced"),
 		icon: "i-lucide-repeat",
+		ui: {
+			itemTrailingIcon: "order-last ml-2",
+		},
 		onSelect: (e: Event) =>
 			updateSettings({
 				...settings.value,
-				defaultEditor: isAdvanced ? "simple" : "advanced",
+				defaultEditor: props.isAdvanced ? "simple" : "advanced",
 			}),
 	};
 
 	const options = [];
-	options.push(isAdvanced ? simple : advanced);
+    //If it's the main editor, add option to open the other one
+	if (props.isAdvanced === (settings?.value?.defaultEditor === "advanced")) {
+		options.push(props.isAdvanced ? simple : advanced);
+	}
+    //Always add option to toggle the default
 	options.push(toggleDefault);
 
 	return options;
+	12;
 });
 </script>
 
@@ -53,7 +67,7 @@ const items = computed<DropdownMenuItem[]>(() => {
 			sideOffset: 8,
 		}"
 		:ui="{
-			content: 'w-12',
+			content: 'w-auto',
 		}"
 	>
 		<UButton icon="i-lucide-menu" color="neutral" variant="ghost" />
