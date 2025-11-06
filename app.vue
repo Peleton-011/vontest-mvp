1f4c750774491a70e75da87d5a8a725cb114f42e
Nico Diaz <nicodiazan@gmail.com>
2025-11-06 10:23:17 +0000

    Iprove mobile styling by adding vertical padding

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ const user = useSupabaseUser();
 </script>
 
 <template>
-	<div class="bg-neutral-900 min-h-screen text-white" data-vaul-drawer-wrapper>
+	<div class="py-6 md:py-0 bg-neutral-900 min-h-screen text-white" data-vaul-drawer-wrapper>
 		<Navbar />
 		<NuxtPage />
 	</div>
